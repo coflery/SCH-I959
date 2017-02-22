aa5cdaf13186639d75d92727470ffa8b759931be
xiaoren <coflery@gmail.com>
2017-02-22 16:49:00 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 12 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#12 SMP PREEMPT Thu Feb 2 01:00:33 CST 2017"
+#define LINUX_COMPILE_BY "xiaoren"
+#define LINUX_COMPILE_HOST "xiaoren-virtual-machine"
+#define LINUX_COMPILER "gcc version 4.7.4 (Ubuntu/Linaro 4.7.4-3ubuntu12) "
